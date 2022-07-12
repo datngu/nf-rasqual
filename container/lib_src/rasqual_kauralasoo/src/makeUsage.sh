a83538f12ae3bcb2f2bd613493ff3ed7d4455707
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-07-12 18:07:52 +0200

    Dockerfile

--- container/lib_src/rasqual_kauralasoo/src/makeUsage.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-grep -v "^#" usage.txt | sed 's/\\$/\\\\/g' | awk -F '\n' 'BEGIN{print "#include<stdio.h>\nvoid usage(){"}{print "\tfprintf(stderr, \""$1"\\n\");"};END{print "}"}' > usage.c
