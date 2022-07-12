98b81b9e0edfc17d8bc0a999528e02f6bfb68394
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-07-12 18:20:36 +0200

    Dockerfile

--- container/build_docker.sh
+++ container/build_docker.sh
@@ -7,13 +7,112 @@ docker tag rasqual:v0.0.0 ndatth/rasqual:v0.0.0
 docker push ndatth/rasqual:v0.0.0
 
 
-
-
 ## testing
 #docker run -it --rm -h datn --name ras ubuntu:18.04
 docker run -it -v /home/datn/github/nf-rasqual:/host/ -h datn --name ras ubuntu:18.04
 
-# install 
+
+##################
+#FROM nfcore/base:2.1
+docker run -it -h datn --name ras nfcore/base:2.1
+#LABEL authors="Dat T Nguyen - ndat<at>utexas.edu" \
+      description="Docker image containing all requirements for running RASQUAL" 
+
+
+#RUN 
+apt-get update && apt-get install -y \
+    make \
+    gcc \
+    wget \
+    tar \
+    libz-dev
+
+# crtl p q
+
+# copy libs
+docker cp lib_src/clapack.tgz ras:clapack-3.2.1.tgz
+docker cp lib_src/gsl-2.5.tar.gz ras:/gsl-2.5.tar.gz
+docker cp lib_src/rasqual_kauralasoo.tgz ras:/rasqual.tgz
+
+docker attach ras
+
+# CLAPACK
+#RUN 
+tar zxvf clapack-3.2.1.tgz
+cd /CLAPACK-3.2.1
+mv make.inc.example make.inc
+#RUN 
+make
+## link file
+#RUN 
+ln -s lapack_LINUX.a liblapack.a
+#RUN 
+ln -s tmglib_LINUX.a libtmglib.a
+#RUN 
+ln -s blas_LINUX.a libblas.a
+cd /
+
+# gsl
+#RUN 
+tar zxvf gsl-2.5.tar.gz
+cd /gsl-2.5
+#RUN 
+./configure --prefix=$PWD
+#RUN 
+make
+#RUN 
+make install
+cd /
+
+# RASQUAL
+ENV CFLAGS -I/CLAPACK-3.2.1/INCLUDE -I/CLAPACK-3.2.1/F2CLIBS -I/gsl-2.5:$CFLAGS
+ENV LDFLAGS -L/CLAPACK-3.2.1 -L/CLAPACK-3.2.1/F2CLIBS -L/gsl-2.5/lib:$LDFLAGS
+ENV LD_LIBRARY_PATH /gsl-2.5/lib:$LD_LIBRARY_PATH
+
+RUN tar zxvf rasqual.tgz
+WORKDIR /rasqual/src
+RUN make
+RUN make install
+WORKDIR /rasqual/src/ASVCF
+RUN make
+RUN make install
+
+WORKDIR /
+ENV PATH /rasqual/src:$PATH
+ENV PATH /rasqual/src/ASVCF:$PATH
+
+# environment for running supporting scripts
+ADD environment.yml /
+RUN conda install mamba -n base -c conda-forge -y
+RUN mamba env create -f /environment.yml
+ENV PATH /opt/conda/envs/rasqual/bin:$PATH
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################### install 
 apt-get update && apt-get install -y \
     make \
     gcc \
