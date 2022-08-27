651d8ca0a25e59c9bee334dac2cd1e6a85060801
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-08-27 20:58:11 +0200

    rename bam

--- bin/rename_bam.R
+++ bin/rename_bam.R
@@ -11,13 +11,13 @@ for( i in 1: nrow(meta)){
     # bam
     new_bam = paste0(meta$genotype_id[i], ".bam")
     old_bam = meta$attac_bam_id[i]
-    cmd = paste0("cp ", old_bam, " ", new_bam)
+    cmd = paste0("cp -L ", old_bam, " ", new_bam)
     print(cmd)
     #system(cmd)
     # bai
     new_bai = paste0(meta$genotype_id[i], ".bam.bai")
     old_bai = paste0(meta$attac_bam_id[i], ".bai")
-    cmd2 = paste0("cp ", old_bai, " ", new_bai)
+    cmd2 = paste0("cp -L ", old_bai, " ", new_bai)
     print(cmd2)
     #system(cmd2)
 }
