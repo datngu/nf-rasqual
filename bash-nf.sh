48765755546b7503fc8f5bc41bbd3009ea8c3701
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-08-27 20:55:21 +0200

    rename bam

---
+++ bash-nf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1                
+#SBATCH --job-name=nf-rasqual   
+#SBATCH --mem=16G                
+#SBATCH --partition=smallmem     
+#SBATCH --mail-user=nguyen.thanh.dat@nmbu.no
+#SBATCH --mail-type=ALL
+
+
+module load BCFtools/1.10.2-GCC-8.3.0
+module load git/2.23.0-GCCcore-9.3.0-nodocs
+module load nextflow/20.10.0
+module load singularity/rpm
+
+
+cd /mnt/users/ngda/proj/paper1/nf-rasqual
+git pull
+
+atac_bam=/mnt/project/Aqua-Faang/seq_results/AtlanticSalmon/BodyMap/ATAC/Brain/results/bwa/mergedLibrary/*{.bam,.bai}
+atac_count=/mnt/project/Aqua-Faang/seq_results/AtlanticSalmon/BodyMap/ATAC/Brain/results/bwa/mergedLibrary/macs/broadPeak/consensus/consensus_peaks.mLb.clN.featureCounts.txt
+genotype=/mnt/users/ngda/ngs_data/atlantic_salmon/wgs/all_chr_added_GP.vcf.gz
+
+outdir=results
+
+nextflow run main.nf -resume --atac_bam $atac_bam --atac_count $atac_count --genotype $genotype --outdir $outdir
