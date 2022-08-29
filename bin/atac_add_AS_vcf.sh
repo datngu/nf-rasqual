4f22cd4f840f29ead7fce23fa31b3780778712ca
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-08-29 10:46:05 +0200

    sort vcf

--- bin/atac_add_AS_vcf.sh
+++ bin/atac_add_AS_vcf.sh
@@ -4,4 +4,8 @@ in_vcf=$1
 out_vcf=$2
 
 ls $PWD/*bam > bam_list.txt
-createASVCF.sh paired_end bam_list.txt $in_vcf $out_vcf atac
+
+bcftools sort $in_vcf -Oz -o sorted.vcf.gz
+bcftools index -tf sorted.vcf.gz
+
+createASVCF.sh paired_end bam_list.txt sorted.vcf.gz $out_vcf atac
