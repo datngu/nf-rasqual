8c4ef0b714beb9d8f39a753aaf3c895f9fe0fc40
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-09-06 10:33:56 +0200

    fix ASVCF

--- draft.sh
+++ draft.sh
@@ -6,17 +6,18 @@ cd /mnt/project/Aqua-Faang/seq_data/AtlanticSalmon/ATAC
 
 ###############################
 # working dir
-module load BCFtools/1.10.2-GCC-8.3.0
+#module load BCFtools/1.10.2-GCC-8.3.0
 module load git/2.23.0-GCCcore-9.3.0-nodocs
 module load nextflow/20.10.0
 module load singularity/rpm
 
 
-cd /mnt/users/ngda/proj/paper1/nf-rasqual
+cd /mnt/SCRATCH/ngda/paper1/nf-rasqual
+
 git pull
 
 atac_bam=/mnt/users/ngda/ngs_data/atlantic_salmon/atac/brain/bam/*{.bam,.bai}
-atac_count=/mnt/project/Aqua-Faang/seq_results/AtlanticSalmon/BodyMap/ATAC/Brain/results/bwa/mergedLibrary/macs/broadPeak/consensus/consensus_peaks.mLb.clN.featureCounts.txt
+atac_count=/mnt/users/ngda/ngs_data/atlantic_salmon/atac/brain/consensus_peaks.mLb.clN.featureCounts.txt
 genotype=/mnt/users/ngda/ngs_data/atlantic_salmon/wgs/sorted_all_chr_added_GP.vcf.gz
 
 outdir=results
