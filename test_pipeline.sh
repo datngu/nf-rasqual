25936e6896ada58bedf3b6d512fc6ff356e487cd
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-10-17 14:46:59 +0200

    gene info parser

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1                
 #SBATCH --job-name=caQTL   
 #SBATCH --mem=4G                
-#SBATCH --partition=orion     
+#SBATCH --partition=hugemem     
 #SBATCH --mail-user=nguyen.thanh.dat@nmbu.no
 #SBATCH --mail-type=ALL
 
