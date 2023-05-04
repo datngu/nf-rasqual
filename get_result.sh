bdc21f95a75a3f2ba53cff9846ddb0766b2f3fd6
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2023-05-04 09:04:12 +0200

    running

---
+++ get_result.sh
@@ -0,0 +1,17 @@
+cd /mnt/ScratchProjects/Aqua-Faang/dat_projects/aqua_qtl/results
+
+rm -rf result_collect_permutation
+mkdir -p result_collect_permutation
+
+
+get_tis (){
+    tis=$1
+    mkdir result_collect_permutation/$tis
+    cp $tis/ATAC_results_emperical_pvalues/rasqual_emperical_pvalues.txt result_collect_permutation/$tis/atac_rasqual_emperical_pvalues.txt
+    cp $tis/RNA_results_emperical_pvalues/rasqual_emperical_pvalues.txt result_collect_permutation/$tis/rna_rasqual_emperical_pvalues.txt
+    cp $tis/deltaSVM_prediction/all_snp_deltaSVM.txt result_collect_permutation/$tis/all_snp_deltaSVM.txt
+}
+
+
+get_tis Brain
+get_tis Liver
