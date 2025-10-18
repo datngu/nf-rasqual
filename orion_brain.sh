870dac1a8f8bbd378823c6933070238e5bffdf59
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2025-10-18 13:51:31 +0200

    add bai

--- orion_brain.sh
+++ orion_brain.sh
@@ -11,31 +11,29 @@ module load Nextflow/21.03
 module load singularity/rpm
 
 
-export TOWER_ACCESS_TOKEN="eyJ0aWQiOiA3OTAxfS4xNGY5NTFmOWNiZmEwNjZhOGFkYzliZTg3MDc4YWI4ZTRiYTk4ZmI5"
-
 
 export NXF_SINGULARITY_CACHEDIR=/mnt/users/ngda/sofware/singularity
 
 
-# download reference genome and annotation
-mkdir -p data/ref
-curl -Lk https://ftp.ensembl.org/pub/release-106/fasta/salmo_salar/dna/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa.gz \
-  -o data/ref/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa.gz
-curl -Lk https://ftp.ensembl.org/pub/release-106/gtf/salmo_salar/Salmo_salar.Ssal_v3.1.106.gtf.gz \
-  -o data/ref/Salmo_salar.Ssal_v3.1.106.gtf.gz
-gunzip -k data/ref/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa.gz
-gunzip -k data/ref/Salmo_salar.Ssal_v3.1.106.gtf.gz
+# # download reference genome and annotation
+# mkdir -p data/ref
+# curl -Lk https://ftp.ensembl.org/pub/release-106/fasta/salmo_salar/dna/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa.gz \
+#   -o data/ref/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa.gz
+# curl -Lk https://ftp.ensembl.org/pub/release-106/gtf/salmo_salar/Salmo_salar.Ssal_v3.1.106.gtf.gz \
+#   -o data/ref/Salmo_salar.Ssal_v3.1.106.gtf.gz
+# gunzip -k data/ref/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa.gz
+# gunzip -k data/ref/Salmo_salar.Ssal_v3.1.106.gtf.gz
 
-# download genotype reference
-curl -Lk https://salmobase.org/datafiles/datasets/nf-rasqual-data/ALL_chrome_phased_filtered_HWE_1e6_biSNPs_MAF_0.01.vcf.gz \
-  -o data/ref/ALL_chrome_phased_filtered_HWE_1e6_biSNPs_MAF_0.01.vcf.gz
+# # download genotype reference
+# curl -Lk https://salmobase.org/datafiles/datasets/nf-rasqual-data/ALL_chrome_phased_filtered_HWE_1e6_biSNPs_MAF_0.01.vcf.gz \
+#   -o data/ref/ALL_chrome_phased_filtered_HWE_1e6_biSNPs_MAF_0.01.vcf.gz
 
-# download ATAC
+# # download ATAC
 
-bash download_atac.sh Brain
+# bash download_atac.sh Brain
 
-# ## download RNA data
-bash download_rna.sh Brain
+# # ## download RNA data
+# bash download_rna.sh Brain
 
 ## results directory
 mkdir -p results/Brain
@@ -56,4 +54,4 @@ nextflow run main.nf -profile orion,singularity \
   --external_ld true \
   --ld_genotype "$PWD/data/ref/ALL_chrome_phased_filtered_HWE_1e6_biSNPs_MAF_0.01.vcf.gz" \
   --atac_qtl true \
-  --eqtl_qtl true
+  --eqtl_qtl true \
